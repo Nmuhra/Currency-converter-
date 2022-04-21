4b95f338c0a04cd909b1478232e43580d8e70cb3
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-04-21 20:42:54 +0200

    Update currency-converter.py

--- currency-converter.py
+++ currency-converter.py
@@ -13,7 +13,15 @@ class RealTimeCurrencyConverter():
         initial_amount = amount 
         if from_currency != 'USD' : 
             amount = amount / self.currencies[from_currency] 
-  
-        # limiting the precision to 4 decimal places 
+
         amount = round(amount * self.currencies[to_currency], 4) 
         return amount
+def __init__(self, converter):
+    tk.Tk.__init__(self)
+    self.title = 'Currency Converter'
+    self.currency_converter = converter
+    self.geometry("500x200")
+    self.intro_label = Label(self, text = 'currency converter',  fg = 'blue', relief = tk.RAISED, borderwidth = 3)
+    self.intro_label.config(font = ('Courier',15,'bold'))
+    self.intro_label.place(x = 10 , y = 5)
+    self.date_label.place(x = 170, y= 50)
